038541b8eaa5a8200553cda772ca41d46e33be22
unknown <jianghubin@jianghubin.spreadtone.com>
2014-07-21 12:44:49 +0800

    first commit

---
+++ c_class/StdAfx.h
@@ -0,0 +1,22 @@
+// stdafx.h : include file for standard system include files,
+//  or project specific include files that are used frequently, but
+//      are changed infrequently
+//
+
+#if !defined(AFX_STDAFX_H__BB0CAA1E_F4A9_4A47_BA17_0D38D1AC03E5__INCLUDED_)
+#define AFX_STDAFX_H__BB0CAA1E_F4A9_4A47_BA17_0D38D1AC03E5__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
+
+#include <stdio.h>
+
+// TODO: reference additional headers your program requires here
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_STDAFX_H__BB0CAA1E_F4A9_4A47_BA17_0D38D1AC03E5__INCLUDED_)
