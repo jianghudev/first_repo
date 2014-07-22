e7c60b17f22a216548b247fdeaf33a196f4f4d2f
jianghudev <jianghubin@163.com>
2014-07-22 11:34:51 +0800

    secound test commit

--- c_class/StdAfx.h
+++ c_class/StdAfx.h
@@ -12,7 +12,9 @@
 
 #define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
 
-#include <stdio.h>
+#include <-stdio.h>
+
+// git test 
 
 // TODO: reference additional headers your program requires here
 
